68026e425f89fcd61c51a30d35b175fc95b9b4ae
praveenkaikala <142200525+praveenkaikala@users.noreply.github.com>
2024-05-02 09:15:45 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,9 +8,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root'),
 );
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
